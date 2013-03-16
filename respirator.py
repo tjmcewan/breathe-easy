9f7414d6b3ffbe7de4f430dfb914ca9dbab1d486
Tim McEwan <tim@mcewan.it>
2013-03-16 22:29:53 +1100

    better naming

--- respirator.py
+++ respirator.py
@@ -1,80 +1,144 @@
 import pdb
 import os
-from flask import Flask, send_from_directory, jsonify, request
-from mask import Mask
+import sys
+import inspect
 
-app = Flask(__name__)
+for subfolder in ["sdk"]:
+    parent = os.path.split(inspect.getfile( inspect.currentframe() ))[0]
+    abs_path = os.path.realpath(os.path.abspath(os.path.join(parent, subfolder)))
+    sys.path.append(abs_path)
 
-api_key = os.environ['O2_API_KEY']
-oxygen_id = os.environ['O2_OXYGEN_ID']
-password = os.environ['O2_PASSWORD']
-testing = app.config['TESTING']
+import o2managementlib
+from o2exceptions import *
 
-app.mask = Mask(api_key, oxygen_id, password, testing)
+class Mask():
 
-
-@app.route('/')
-def index():
-    return 'Welcome to Respirator!  Take a deep breath and relax.'
-
-
-@app.route('/spaces', methods=['POST'])
-def spaces():
-    try:
-        name = request.json['name']
-        space = app.mask.create_space(name)
-        return jsonify(space), 200
-    except KeyError:
-        error = {"error": "ensure parameters include a 'name' key"}
-        return jsonify(error), 400
-
-@app.route('/users', methods=['GET', 'POST'])
-def users():
-    if request.method == 'GET':
-        users = app.mask.get_users()
-        return jsonify(users)
-    else:
+    def __init__(self, api_key, oxygen_id, password, testing):
+        self.o2 = o2managementlib.O2AgentFactory().create_O2Agent(api_key)
         try:
-            attributes = request.json['user']
-            user, status_code = app.mask.create_user(attributes)
-            return jsonify(user), status_code
-        except KeyError:
-            error = {"error": "ensure parameters include a 'user' key"}
-            return jsonify(error), 400
+            if testing == False:
+                self.o2.login_api_user(oxygen_id, password)
+        except O2Exception as error:
+            print error.message
+            sys.exit(1)
 
-@app.route('/spaces/<space>', methods=['GET', 'PUT', 'PATCH', 'DELETE'])
-def space(space):
-    if request.method == 'GET':
-        space, status_code = app.mask.show_space(space)
-        return jsonify(space), status_code
-    elif request.method == 'DELETE':
-        result = app.mask.delete_space(space)
-        return jsonify(result)
-    else:
-        space = app.mask.update_space(space, request.json)
-        return jsonify(space)
+    # def find_user(self, oxygen_id):
+    #     return self.o2.get_user_by_oxygen_id(oxygen_id)
 
-@app.route('/spaces/<space>/subscriptions', methods=['GET', 'POST'])
-def subscriptions(space):
-    if request.method == 'GET':
-        subscriptions = app.mask.subscriptions(space)
-        return jsonify(subscriptions)
-    else:
-        subscription = app.mask.create_subscription(space, request.json)
-        return jsonify(subscription)
+    # def subscriptions_for_user(self, oxygen_id):
+    #     # oxygen_id = self.find_user(oxygen_id)
+    #     return self.o2.get_subscriptions_for_user(oxygen_id)
 
-@app.route('/spaces/<space>/subscriptions/<subscription>', \
-    methods=['GET', 'DELETE'])
-def subscription(space, subscription):
-    if request.method == 'GET':
-        subscription = app.mask.subscription(request.json)
-        return jsonify(subscription)
-    else:
-        result = app.mask.destroy_subscription(request.json)
-        return jsonify(result)
+    def get_all_users(self):
+        self.o2
+
+    def create_space(self, space_name):
+        attributes = [
+            space_name,
+            'description goes here',
+            'tim@mcewan.it',
+            '',
+            0,
+            True,
+            True
+        ]
+        space = self.o2.create_space(*attributes)
+        attributes = self.get_space_attributes(space)
+        return {'space': attributes}
+
+    def show_space(self, space_name):
+        try:
+            space = self.o2.get_space_by_space_name(space_name)
+        except O2SystemError as error:
+            return {space_name: error.message}, 404
+
+        attributes = self.get_space_attributes(space)
+        return {'space': attributes}, 200
+
+    def delete_space(self, space_name):
+        try:
+            space = self.o2.get_space_by_space_name(space_name)
+        except O2SystemError as error:
+            return {space_name: error.message}
+
+        space_id = space.get_space_id()
+        try:
+            result = self.o2.delete_space(space_id)
+        except O2SystemError as error:
+            return {space_name: error.message}
+
+        return {'result': result}
 
 
-if __name__ == '__main__':
-    app.debug = True
-    port = int(os.environ.get('PORT', 3000))
-    app.run('0.0.0.0', port)
+    def update_space(self, arg):
+        pass
+
+    def subscriptions(self, arg):
+        pass
+
+    def create_subscription(self, arg):
+        pass
+
+    def subscription(self, arg):
+        pass
+
+    def destroy_subscription(self, arg):
+        pass
+
+    def subscriptions_for_user(self, arg):
+        pass
+
+    def get_space_attributes(self, space):
+        return {
+            'id': space.get_space_id(),
+            'name': space.get_name(),
+            'description': space.get_description(),
+            'owner_id': space.get_owner_oxygen_id(),
+            'storage_name': space.get_storage_name(),
+            'utilization': space.get_utilized(),
+            'listed_in_space_directory': space.is_listed(),
+            'writable_by_default': space.is_writable_by_default()
+        }
+
+
+    def create_user(self, attributes):
+        try:
+            ordered_attributes = [
+                attributes['oxygen_id'],
+                attributes['email'],
+                attributes.get('corporate_user_name', ''),
+                attributes['first_name'],
+                attributes['last_name'],
+                attributes.get('password', '')
+            ]
+        except KeyError as error:
+            return {'missing_key': error}, 400
+
+        try:
+            user = self.o2.create_user(*ordered_attributes)
+        except O2InvalidInputError as error:
+            return {attributes['oxygen_id']: error.message}, 400
+
+        attributes = self.get_user_attributes(user)
+        return {'user': attributes}, 200
+
+    def get_users(self):
+        users = self.o2.get_all_users()
+        users_attributes = map(self.get_user_attributes, users)
+        return {'users': users_attributes}
+
+
+    def get_user_attributes(self, user):
+        return {
+            'oxygen_id': user.get_oxygen_id(),
+            'first_name': user.get_first_name(),
+            'last_name': user.get_last_name(),
+            'display_name': user.get_display_name(),
+            'email': user.get_email(),
+            'external?': user.is_external(),
+            'deleted?': user.is_deleted(),
+            'disabled?': user.is_disabled(),
+            'last_login': user.get_last_login_date_time(),
+            'external_id': user.get_external_id(),
+            'not_yet_activated?': user.is_not_activated()
+        }
